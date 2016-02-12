c502cc505a00c76c73f6e7fa3bca8ac88df84f04
Eduardo Elizondo <eduardo.elizondorueda@gmail.com>
2016-02-12 17:08:13 -0600

    Finished testing python scripts

--- testelectrodes.py
+++ testelectrodes.py
@@ -1,24 +1,27 @@
 import os
+import sys
 import json
 
 import numpy as np
 from readbytes import _read8, _read32
+from datafilters import apply_dc_filter
 
 dt_f32 = np.dtype("<f4")
 electrodes = [False, False, False, False, False, False, False, False]
 
 #Gather data from Hardware
 #Get data from hardware
-fileloc = "./../lib/start_stream.py"
-dumploc = "../../modeling"
+#fileloc = "./../lib/start_stream.py"
+#dumploc = "../../modeling"
 masterfolder = "testing"
 samplefolder = "sample"
-sampletime = 2 #sec
-sampleid = 0
-os.system("python3 ./../lib/start_stream.py ../../modeling/ 2 testing sample 0")
+#sampletime = 2 #sec
+#sampleid = 0
+#os.system("python3 ./../lib/start_stream.py ../../modeling/ 2 testing sample 0")
 
 #Read data from Hardware
-testingpath = "./"+ masterfolder + "/" + samplefolder
+path = os.path.abspath(os.path.join(__file__,sys.argv[1]))
+testingpath = path + "/"+ masterfolder + "/" + samplefolder
 filename = os.listdir(testingpath)[0]
 
 max_rows = 470
@@ -38,5 +41,21 @@ for i in range(0,data.shape[1]):
   if max(data[:,i]) < max_thresh and min(data[:,i]) > min_thresh:
     electrodes[i] = True
 
-with open("testing/out.log", "w") as writestream:
+#DC Filter
+fs = 250
+enable_dc = True
+dc_lowcut = 8.0 #Only get alpha and beta, most related to movement
+dc_highcut = 30.0
+dc_order = 2
+dc_type = "bandpass"
+dc_func_type = "butter"
+
+data.flags['WRITEABLE'] = True
+for i in range(0,data.shape[1]):
+  if enable_dc:
+    data[:,i] = apply_dc_filter(data[:,i], fs, dc_lowcut, dc_highcut, dc_order, dc_type, dc_func_type)
+
+np.savetxt(path + "/../public/data/test.csv", data, delimiter=",",newline="\n")
+
+with open(path+"/testing/out.log", "w") as writestream:
   writestream.write(json.dumps({"electrodes": electrodes}))
